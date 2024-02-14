fa915495b0848463b324c144c24e97bc14d524c9
ejrober <25645445+ejrober@users.noreply.github.com>
2024-02-14 08:58:55 -0700

    Changes to question 3 working on printing positions of elements from ordered array.

--- homework5/question3.cpp
+++ homework5/question3.cpp
@@ -5,31 +5,41 @@ using namespace std;
 void mostPopularWord(string words[], const int WORD_SIZE);
 
 int main(){
-    string words[] = {"this", "is", "a", "test"};
+    string words[] = {"mail", "text", "spam", "spam"};;
     const int WORD_SIZE = 4;
     mostPopularWord(words, WORD_SIZE);
 }
 
 void mostPopularWord(string words[], const int WORD_SIZE){
-    // Mark all array elements as not visited
-    vector<bool> visited(WORD_SIZE, false);
 
-    // Traverse the array elements and count frequencies
+    int frequencies[sizeof(WORD_SIZE)] = {0};
+    int max_index = 0;
+
+    sort(words, words + WORD_SIZE); // sort array for binary search
+
     for(int i = 0; i < WORD_SIZE; i++){
-        //Skip this element if already processed
-        if(visited[i] == true){
-            continue;
-        }
+        // index of first and last occurance of arr[i]
+        int first_index = lower_bound(words, words + WORD_SIZE, words[i]) - words;
+        int last_index = upper_bound(words, words + WORD_SIZE, words[i]) - words - 1;
+        i = last_index;
 
-        //Count frequency
-        int count = 1;
-        for(int j = i + 1; j < WORD_SIZE; j++){
-            if(words[i] == words[j]){
-                visited[j] = true;
-                count++;
-            }
+        int frequency = last_index - first_index + 1; // finding frequency
+        frequencies[i] = frequency;
+        cout << words[i] << " " << frequencies[i] << endl;
+
+        if(frequencies[max_index] < frequencies[i]){
+            max_index = i;
+
         }
-        cout << words[i] << " " << count << endl;
     }
-    cout << "The most popular word is " << words[0] << endl;
+    cout << "The most popular word is : " << words[max_index] << endl;
+    cout << "Frequency : " << frequencies[max_index] << endl;
+    
+    for(int i = 0; i < WORD_SIZE; i ++){
+        if(words[i] == words[max_index]){
+            cout  << "Found at pos : " << i << endl;
+        }
+        
+    }  
+    
 }
