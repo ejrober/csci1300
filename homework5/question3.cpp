c06e715ebb79a3d41a9c548c653a3631dead5866
ejrober <25645445+ejrober@users.noreply.github.com>
2024-02-15 10:16:46 -0700

    Question 3 of homework5 completed. Program prints the most popular word of an array, its frequency and the indices it appears in.

--- homework5/question3.cpp
+++ homework5/question3.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
-#include <bits/stdc++.h>
+
 using namespace std;
 
 void mostPopularWord(string words[], const int WORD_SIZE);
 
 int main(){
-    string words[] = {"mail", "text", "spam", "spam"};;
-    const int WORD_SIZE = 4;
+    string words[] = {"apple", "corn", "corn", "apple", "lettuce"};
+    const int WORD_SIZE = 5;
     mostPopularWord(words, WORD_SIZE);
+
+    const int WORDS_SIZE1 = 4;
+    string words1[WORDS_SIZE1] = {"mail", "text", "spam", "spam"};
+    mostPopularWord(words1, WORDS_SIZE1);
+
+    const int WORDS_SIZE2 = 6;
+    string words2[WORDS_SIZE2] = {"apple", "apple", "foo", "bar", "apple", "code"};
+    mostPopularWord(words2, WORDS_SIZE2);
+    return 1;
 }
 
 void mostPopularWord(string words[], const int WORD_SIZE){
@@ -15,31 +24,33 @@ void mostPopularWord(string words[], const int WORD_SIZE){
     int frequencies[sizeof(WORD_SIZE)] = {0};
     int max_index = 0;
 
-    sort(words, words + WORD_SIZE); // sort array for binary search
-
     for(int i = 0; i < WORD_SIZE; i++){
-        // index of first and last occurance of arr[i]
-        int first_index = lower_bound(words, words + WORD_SIZE, words[i]) - words;
-        int last_index = upper_bound(words, words + WORD_SIZE, words[i]) - words - 1;
-        i = last_index;
+        //cout << "The word being processed is " << words[i] << endl;
+        int count = 1;
 
-        int frequency = last_index - first_index + 1; // finding frequency
-        frequencies[i] = frequency;
-        cout << words[i] << " " << frequencies[i] << endl;
-
-        if(frequencies[max_index] < frequencies[i]){
-            max_index = i;
-
-        }
-    }
-    cout << "The most popular word is : " << words[max_index] << endl;
-    cout << "Frequency : " << frequencies[max_index] << endl;
-    
-    for(int i = 0; i < WORD_SIZE; i ++){
-        if(words[i] == words[max_index]){
-            cout  << "Found at pos : " << i << endl;
+        for(int j = i +1; j < WORD_SIZE; j++){
+            if(words[i] == words[j]){
+                count++;
+            }
         }
         
-    }  
+        frequencies[i] = count;
+        //cout << "The frequency of " << words[i] << " is " << frequencies[i] << endl;
+        if(frequencies[i] > frequencies[max_index]){
+            max_index= i;
+            //cout << "The index of the word with the maximum frequency int the array is " << max_index << endl;
+        }
+    }
+
+    cout << "The most popular word is : " << words[max_index]  << endl;
+    cout << "Frequency : " << frequencies[max_index] << endl;
+
+    cout << "Found at pos : " << endl;
+
+    for(int i = 0; i < WORD_SIZE; i ++){ // This loop prints the indices of the most popular word in the array.
+        if(words[i] == words[max_index]){
+            cout << i << endl;
+        }
+    }
     
 }
